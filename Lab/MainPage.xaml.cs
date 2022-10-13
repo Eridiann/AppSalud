2547f2c49aa200c0ea08f1fd2677edf6d0c0b9d6
anonimus <anonimus@nowhere.com>
2022-10-12 19:08:22 -0500

    Register button

--- Lab/MainPage.xaml.cs
+++ Lab/MainPage.xaml.cs
@@ -1,31 +1,109 @@
-﻿using Lab.Pages;
+﻿using Lab.Models;
+using Lab.Pages;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Lab
 {
     public partial class MainPage : ContentPage
     {
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
         public MainPage()
         {
             InitializeComponent();
             btnEntrar.Clicked += BtnEntrar_Clicked;
+            btnRegistrar.Clicked += BtnRegistrar_Clicked;
         }
 
-        private void BtnEntrar_Clicked(object sender, EventArgs e)
+        private async void BtnRegistrar_Clicked(object sender, EventArgs e)
         {
-            if(txtNombre.Text == null || txtNombre.Text.Length == 0)
+            if (txtNombre.Text == null || txtNombre.Text.Length == 0 ||
+                txtPassword == null || txtPassword.Text.Length == 0 ||
+                txtEmail == null || txtEmail.Text.Length == 0)
             {
-                DisplayAlert("Error", "Captura tu nombre", "OK");
+                await DisplayAlert("Error", "Captura tu nombre", "OK");
             }
             else
             {
-                Navigation.PushAsync(new DataPage(txtNombre.Text.ToString()));
+                Usuario usuario = new Usuario
+                {
+                    username = txtNombre.Text,
+                    password = txtPassword.Text,
+                    email = txtEmail.Text,
+                };
+
+                string json = JsonConvert.SerializeObject(usuario);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                string url = "https://saludomar.azurewebsites.net/api/cuentas/registrar";
+
+                var response = await _httpClient.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    Token token = new Token();
+                    token = JsonConvert.DeserializeObject<Token>(result);
+
+                    string miToken = token.TokenToken.ToString();
+                    await SecureStorage.SetAsync("Token", miToken);
+                    await Navigation.PushAsync(new DataPage(txtNombre.Text.ToString()));
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Credenciales invalidas o servidor inaccesible", "OK");
+                }
+
+            }
+        }
+
+        private async void BtnEntrar_Clicked(object sender, EventArgs e)
+        {
+            if(txtNombre.Text == null || txtNombre.Text.Length == 0 ||
+                txtPassword == null || txtPassword.Text.Length == 0 ||
+                txtEmail == null || txtEmail.Text.Length == 0)
+            {
+                await DisplayAlert("Error", "Captura tu nombre", "OK");
+            }
+            else
+            {
+                Usuario usuario = new Usuario
+                {
+                    username = txtNombre.Text,
+                    password = txtPassword.Text,
+                    email = txtEmail.Text,
+                };
+
+                string json = JsonConvert.SerializeObject(usuario);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                string url = "https://saludomar.azurewebsites.net/api/cuentas/login";
+
+                var response = await _httpClient.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    Token token = new Token();
+                    token = JsonConvert.DeserializeObject<Token>(result);
+
+                    string miToken = token.TokenToken.ToString();
+                    await SecureStorage.SetAsync("Token", miToken);
+                    await Navigation.PushAsync(new DataPage(txtNombre.Text.ToString()));
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Credenciales invalidas o servidor inaccesible", "OK");
+                }
+
             }
         }
     }
---
+++ Lab/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab.Models
+{
+    internal class Usuario
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
